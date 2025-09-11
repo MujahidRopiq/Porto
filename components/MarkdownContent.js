bc22c9a9198159efdc6b4d2502fa22e0bcf03367
murof2003@student.uns.ac.id <Mujahid.murof2003@student.uns.ac.id>
2025-09-11 11:41:17 +0700

    feat: worklist

--- components/MarkdownContent.js
+++
@@ -1,8 +0,0 @@
-export default function MarkdownContent({ content }) {
-  return (
-    <div
-      className="markdown-content"
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
-  );
-}
